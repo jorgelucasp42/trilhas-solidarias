c2f1095cce3b6ff039b3c319cf7d7ace64271fbf
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-21 16:22:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Trilhas Solidárias",
+  "/login": "Entrei na rota login",
+  "/cadastro": "Entrei na rota cadastro"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Trilhas Solidárias.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
