8d194369e7860400cd9e209bec342520feb80fb4
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-22 12:39:33 -0300

    update

--- src/app.js
+++ src/app.js
@@ -16,38 +16,6 @@ const app = express();
 routes(app);
 
 
-app.get("/usuarios/:id", (req, res) => {
-    const index = buscaUsuario(req.params.id);
-    if (index !== -1) {
-        res.status(200).json(usuarios[index]);
-    } else {
-        res.status(404).send("Usuario não encontrado");
-    }
-});
-
-app.post("/usuarios", (req, res) => {
-    const novoUsuario = req.body;
-    // Validação para evitar IDs duplicados
-    const idExiste = usuarios.some(usuario => usuario.id === novoUsuario.id);
-    if (idExiste) {
-        res.status(400).send("ID de usuario já existe");
-    } else {
-        usuario.push(novoUsuario);
-        res.status(201).send("Usuario cadastrado com sucesso");
-    }
-});
-
-app.put("/usuarios/:id", (req, res) => {
-    const id = req.params.id;
-    const index = buscaUsuario(id);
-    if (index !== -1) {
-        usuarios[index] = { ...usuarios[index], ...req.body };  // Atualiza o usuário mantendo o ID
-        res.status(200).send("Usuario atualizado com sucesso");
-    } else {
-        res.status(404).send("Usuario não encontrado");
-    }
-});
-
 app.delete("/usuarios/:id", (req, res) => {
     const id = req.params.id;
     const index = buscaUsuario(id);
@@ -59,14 +27,5 @@ app.delete("/usuarios/:id", (req, res) => {
     }
 });
 
-app.patch("/usuarios/:id", (req, res) => {
-    const id = req.params.id;
-    const index = buscaUsuario(id);
-    if (index !== -1) {
-        usuarios[index] = { ...usuarios[index], ...req.body };  // Atualiza os campos fornecidos
-        res.status(200).send("Usuario atualizado parcialmente com sucesso");
-    } else {
-        res.status(404).send("Usuario não encontrado");
-    }
-});
+
 export default app;
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -29,6 +29,18 @@ class UsuarioController{
         }
     }
 
+    static async atualizarUsuario (req, res) {
+        try {
+          const id = req.params.id;
+          await usuario.findByIdAndUpdate(id, req.body);
+          res.status(200).json({ message: "Usuario atualizado" });
+        } catch (erro) {
+          res.status(500).json({ message: `${erro.message} - Falha na atualização` });
+        }
+      };
+
+
 }
+
 
 export default UsuarioController;
