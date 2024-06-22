73367982bc90fb342494dfa87410c1f6fb12f009
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-22 12:30:23 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,21 +1,3 @@
-//import mongoose from "mongoose";
-
-// async function conectaNaDatabase() {
-//     try {
-//         await mongoose.connect(process.env.DB_CONNECT_STRING, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             serverSelectionTimeoutMS: 3000  // Adiciona um tempo limite de 3 segundos
-//         });
-//         console.log("Conexão com o banco feita com sucesso");
-//         return mongoose.connection;
-//     } catch (error) {
-//         console.error("Erro de conexão:", error);
-//         throw error;
-//     }
-// }
-
-// export default conectaNaDatabase;
 
 import mongoose, { mongo } from "mongoose";
 
@@ -25,26 +7,3 @@ async function conectaNaDatabase() {
 };
 
 export default conectaNaDatabase;
-
-
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
-// dotenv.config();  // Certifique-se de que as variáveis de ambiente estão carregadas
-
-// async function conectaNaDatabase() {
-//     try {
-//         await mongoose.connect(process.env.DB_CONNECT_STRING, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-//         console.log("Conexão com o banco feita com sucesso");
-//         return mongoose.connection;
-//     } catch (error) {
-//         console.error("Erro de conexão", error);
-//         throw error;
-//     }
-// };
-
-// export default conectaNaDatabase;
-
