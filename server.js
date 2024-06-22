952e3148638c34571fec04be53d47886e037e83e
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-21 23:05:25 -0300

    update

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-//import http from "http";
 
 import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-  "/": "Trilhas Solidarias",
-  "/login": "Entrei na rota login",
-  "/cadastro": "Entrei na rota cadastro"
-};
-
-//const server = http.createServer((req, res) => {
-//  res.writeHead(200, { "Content-Type": "text/plain" });
-//  res.end(rotas[req.url]);
-//});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from "express";
 
 const app = express ();
+app.use(express.json()); 
+
+const usuario = [
+    {
+        id: 1,
+        nome: "Xavier"
+    },
+    {
+        id: 2,
+        nome: "JMagneto"
+    }
+];
+
+
 
 app.get("/", (req, res) => {
     res.status(200).send("Trilhas Solidarias.js");
   });
 
   export default app; 
+
