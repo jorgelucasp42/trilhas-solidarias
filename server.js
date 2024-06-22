ba665a0195533d42bfdf3f01377a8d8bcd38eed8
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-06-22 11:26:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import app from "./src/app.js";
 const PORT = 3000;  
 
 app.listen(PORT, () => {
-  console.log("servidor escutando na porta ${PORT}!");
+  console.log(`Servidor escutando na porta ${PORT}`);
 });
