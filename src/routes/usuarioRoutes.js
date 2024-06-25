752bbfd0c1028f86862ad926c37900e0aeca102a
paulohrsodre <paulohrsodre@hotmail.com>
2024-06-24 21:31:43 -0300

    ajustando conexão com API

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import UsuarioController from "../controllers/usuarioController.js";
+import authenticate from "../middleware/auth.js";
 
 const routes = express.Router();
 
-routes.get("/usuarios", UsuarioController.listarLivros);
+routes.get("/usuarios", UsuarioController.listarUsuarios);
+routes.post("/usuarios", authenticate, UsuarioController.cadastrarUsuario);
+routes.post("/login", authenticate, UsuarioController.loginUsuario);
 
 export default routes;
